832b530970c68dedee5c7d8082563acfc4a2c4c9
Genelle Pugliese <Genelle@GENELLEs-MBP.attlocal.net>
2016-01-26 21:50:12 -0500

    Print tasks to screen and other changes
    Print tasks to screen
    fixed a bug in new_user
    added more info to read

--- readme.txt
+++ readme.txt
@@ -1,3 +1,14 @@
 Temptation Bundling app - help break bad habits by bundling them with a reward
 
-Still very much a work in process!!
+This is the first full-fledged (i.e. non-kata or homework) program that I have written. The hope is (someday) that this will be a cross-platform app, but for now I'm keeping it on the command line so I can just focus on the development. Any suggestions/tips are welcome and appreciated!
+
+To-do for current state of program (command line):
+* need to create table to track what habits users have completed and what rewards they have cashed in
+* set up cycling of points to "savings" for higher point amount rewards
+* set up menu to choose from after establishing user id, and the functions to go along with them
+* write up full documentation
+
+Future ideas/to-do's:
+* incorporate a gui to desktop app (wxPython?)
+* learn to develop web apps, iOS, android, etc (maybe use kivy?)
+* get better at programming! :-P
--- tempBundling.py
+++ tempBundling.py
@@ -15,17 +15,32 @@ def main():
             cur.execute('SELECT username from users where username = ? ', (username, ) )
             db_username = cur.fetchone()
 
-            if db_username[0] == username:
-                break
-                
-            else:
+
+            if db_username == None:
                 print "I'm sorry, I didn't recognize your username."
                 continue
+                
+            else:
+                break
     
     print "Hi, %s, let's get started!" %username
+    print "The following are the tasks you"
+    print "have indicated you want to track."
+    
+    print_tasks(username)
+    print "--------------------------"
+    print "\n"
+    
+    #put while loop here, asks user what they would like to do
+    #write up different functions for each option
+    task_to_update = raw_input("So what task would you like to update?")
+    
 
 
 def new_user():
+    """
+    Creates a new user in the database
+    """
     print "Then let's set you up with your account!"
     
     while(True):
@@ -43,6 +58,30 @@ def new_user():
             print "I'm sorry, that username name is taken. Please try again."
             continue
     return username
+    
+def print_tasks(username):
+    """
+    Prints habits/tasks that the user has indicated they want to track
+    """
+
+    cur.execute('SELECT user_id from users where username = ? ', (username, ) )
+    db_user_id = cur.fetchone()[0]  
+    #Should probably test here that user_id isn't None but I don't run this 
+    #unless I have a valid username so it should be okay?
+    
+    cur.execute('SELECT habit_id from user_habit where user_id = ? ', (db_user_id, ))
+    db_tasks = cur.fetchall()
+    
+    #Stack Overflow, ljust tabular output
+    print "{} {}".format("Tasks:".ljust(20),"Description:".ljust(20))
+    for habit_id in db_tasks:
+
+        cur.execute('SELECT habit_name, habit_desc from habits where habit_id = ?', (habit_id[0], ))
+        db_habit = cur.fetchone()
+        
+        print "{} {}".format(db_habit[0].ljust(20),db_habit[1].ljust(20))
+    
+    
     
 main()
 
